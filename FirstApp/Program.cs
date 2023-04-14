b6603766b60d6f4c2af55cb3855f52d6fdcddead
VSKuznec <sevactet.games@gmail.com>
2023-04-14 22:06:24 +0300

    VS Shit

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,7 +1,12 @@
 ﻿static void Main(string[] args);
-{
-    string MyName;
-    MyName = "Jane";
-    Console.WriteLine(MyName);
-    Console.ReadKey();
-}
+        {
+            const string MyName = "Jane";
+
+            Console.WriteLine(MyName);
+
+            Console.WriteLine("\t Привет, мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n Jane");
+
+            Console.ReadKey();
+        }
