5ebbb79b2f6c2a339ed5685116f9079d8f4fafce
VSKuznec <sevactet.games@gmail.com>
2023-04-15 18:45:29 +0300

    TRUE FALSE WORK

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 
-class MainClass{
-    public static void Main(string[] args) {
-
-            const string MyName = "Jane";
-
-            Console.WriteLine(MyName);
-
-            Console.WriteLine("\t Привет, мир");
-            Console.WriteLine("\t Мне 27 лет");
-            Console.WriteLine("\t My name is \n {0}", MyName);
-            Console.WriteLine("\u0040");
-            Console.WriteLine("\u0023");
-            Console.ReadKey();
-
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(true);
+        Console.WriteLine(false);
+        Console.WriteLine(5);
     }
 }
