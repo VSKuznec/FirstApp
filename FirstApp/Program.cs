b3bbfe6167b4e94977c2a6584c7da9ea462e2681
VSKuznec <sevactet.games@gmail.com>
2023-04-15 17:52:57 +0300

    Symbols

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,15 +1,18 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-        {
+﻿using System;
+
+class MainClass{
+    public static void Main(string[] args) {
+
             const string MyName = "Jane";
 
             Console.WriteLine(MyName);
 
             Console.WriteLine("\t Привет, мир");
             Console.WriteLine("\t Мне 27 лет");
-            Console.WriteLine("\t My name is \n Jane");
-
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\u0023");
             Console.ReadKey();
-        }
+
     }
+}
