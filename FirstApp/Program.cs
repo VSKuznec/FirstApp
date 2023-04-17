18cdd977affa1963395863fb7f9f3a59325a36ad
VSKuznec <sevactet.games@gmail.com>
2023-04-17 13:33:30 +0300

    int values

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -3,15 +3,7 @@
 class MainClass {
     private static void Main(string[] args)
         {
-            string MyName = "Jane";
-            byte MyAge = 27;
-            bool HaveIApet = true;
-            double MyShoeSize = 37.5;
-
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("My age is " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HaveIApet);
-            Console.WriteLine("My shoe size is " + MyShoeSize);
-
+        Console.WriteLine("IntMin {0} ", int.MinValue);
+        Console.WriteLine("IntMax {0} ", int.MaxValue);
         }
     }
