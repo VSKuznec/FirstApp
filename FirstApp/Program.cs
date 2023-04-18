f15a9d2d089e2f96432277f0ecfb8f99aa41caea
VSKuznec <sevactet.games@gmail.com>
2023-04-18 18:59:55 +0300

    Add New Form

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 
-class MainClass {
+class Program {
     private static void Main(string[] args)
-        {
-            string MyName = "Jane";
-            byte MyAge = 27;
-            bool HaveIApet = true;
-            double MyShoeSize = 37.5;
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter your age: ");
+        byte age = checked((byte)int.Parse(Console.ReadLine()));
+        Console.WriteLine("Your name is {0} and age is {1}", name, age);
 
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("My age is " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HaveIApet);
-            Console.WriteLine("My shoe size is " + MyShoeSize);
+        Console.WriteLine("What is your favorite day of week?");
+        DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your favorite day is {0}", day);
 
-        }
+        Console.ReadKey();
     }
+}
